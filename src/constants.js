244c751bf16fa58d9a57f950b8021ca2deede89b
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-07-04 16:58:02 +0200

    build: remove chalk dependency

--- src/constants.js
+++ src/constants.js
@@ -1,15 +1,9 @@
 'use strict'
 
-const chalk = require('chalk')
-
 const { API_KEY, PORT = 1337, NODE_ENV = 'development' } = process.env
 
 if (!API_KEY) {
-  console.log(
-    `${chalk.yellow(
-      'warning'
-    )} \`API_KEY\` not set, anyone can increment your counters.`
-  )
+  console.warn(`\`API_KEY\` not set, anyone can increment your counters.`)
 }
 
 const isProduction = NODE_ENV === 'production'
